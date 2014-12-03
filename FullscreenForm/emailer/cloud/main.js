9feb8d42b97c710ca08e59143fe3c889619ed838
ibrahimrahman <ibrahim_rahman@outlook.com>
2014-12-03 19:14:13 +0500

    udpate

--- FullscreenForm/emailer/cloud/main.js
+++ FullscreenForm/emailer/cloud/main.js
@@ -1,33 +1,30 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
+/* global Parse,console,require */
 
-var API = "key-be08724fb28a4bf6a1e3cbfb56374006"
-var Domain = "sandbox9a3dfe10d19f48b487cd88771cb89057.mailgun.org"
 var Mailgun = require('mailgun');
-Mailgun.initialize(Domain, API);
- 
-Parse.Cloud.beforeSave("emailObject", function(request, response) {
- 
-    var text = "Test form\n\n" + 
-        "Name: "+request.object.get("q1") + "\n\n"+
-        "Email: "+request.object.get("q2") + "\n\n"+
-        "Message: \n"+request.object.get("q3");
- 
-     
-    Mailgun.sendEmail({
-            to: "ibrahim_rahman@outlook.com",
-            from: request.object.get("email"),
-            subject: "Submission Form ibrahimrahman.github.io from: " + request.object.get("name"),
-            text: text
-        }, {
-        success: function(httpResponse) {
-            response.success();
-        },
-        error: function(httpResponse) {
-            console.error(httpResponse);
-            response.error("Uh oh, something went wrong");
-        }
-    });
- 
+Mailgun.initialize('andbox9a3dfe10d19f48b487cd88771cb89057.mailgun.org', 'key-be08724fb28a4bf6a1e3cbfb56374006');
+
+Parse.Cloud.beforeSave("CommentObject", function(request, response) {
+
+	var text = "Comment Email\n" + 
+		"From: "+request.object.get("name") + "\n"+
+		"Email: "+request.object.get("email") + "\n"+
+		"Area: "+request.object.get("area") + "\n\n"+
+		"Comments:\n" + request.object.get("comments");
+	
+	Mailgun.sendEmail({
+			to: "ibrahim_rahman@outlook.com",
+			from: request.object.get("email"),
+			subject: "Comment Form - " + request.object.get("area"),
+			text: text
+		}, {
+		success: function(httpResponse) {
+			response.success();
+		},
+		error: function(httpResponse) {
+			console.error(httpResponse);
+			response.error("Uh oh, something went wrong");
+		}
+	});
+
 });
